d05373317008623e2e08952f5c891434eec993ae
Gomsi <gomsi1020@gmail.com>
2022-04-10 15:00:18 +1000

    game done

--- scripts/player.gd
+++ scripts/player.gd
@@ -28,3 +28,53 @@ func read_input():
 
 func _physics_process(delta):
 	read_input()
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		
+		# Level 1
+		if collision.collider.name.begins_with("Statue"):
+			var dialog = Dialogic.start("Statue_clue")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Well"):
+			var dialog = Dialogic.start("Well_clue")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Chest"):
+			var dialog = Dialogic.start("Chest_clue")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Sign"):
+			var dialog = Dialogic.start("Sign_start")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Portal"):
+			var dialog = Dialogic.start("Portal")
+			add_child(dialog)
+		
+		# Level 2
+		if collision.collider.name.begins_with("Guidance1"):
+			var dialog = Dialogic.start("Guidance1")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Guidance2"):
+			var dialog = Dialogic.start("Guidance2")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Alien1"):
+			var dialog = Dialogic.start("Alien1")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Alien2"):
+			var dialog = Dialogic.start("Alien2")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Alien3"):
+			var dialog = Dialogic.start("Alien3")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Doorway"):
+			var dialog = Dialogic.start("Doorway")
+			add_child(dialog)
+			
+		# Level 3
+		if collision.collider.name.begins_with("Marker"):
+			var dialog = Dialogic.start("Marker")
+			add_child(dialog)
+		if collision.collider.name.begins_with("Final"):
+			var dialog = Dialogic.start("Final")
+			add_child(dialog)
+		
+		
